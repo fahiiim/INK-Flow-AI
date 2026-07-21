9a245e2fd5650dfa47309c2b2a97a8180329c8a0
fahiiim <fahimsarker0805@gmail.com>
2026-07-21 10:53:41 +0600

    Host IP Changed

--- api/__main__.py
+++ api/__main__.py
@@ -6,7 +6,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
-        host="127.0.0.1",
+        host="10.10.28.89",
         port=8001,
         reload=True,
     )
