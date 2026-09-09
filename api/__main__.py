2b616637abd0acdf9b6560adf73d0da8cb666fe6
fahiiim <fahimsarker0805@gmail.com>
2026-09-09 11:36:39 +0600

    Intake Low Risk Question

--- api/__main__.py
+++ api/__main__.py
@@ -6,7 +6,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
-        host="0.0.0.0",
+        host="10.10.28.179",
         port=8001,
         reload=False,
     )
